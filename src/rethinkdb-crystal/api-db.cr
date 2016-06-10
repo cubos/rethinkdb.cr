6e6772359372af9fc19d4e07b653a93d53beb502
Guilherme Bernal <dev@lbguilherme.com>
2016-06-10 18:55:36 -0300

    Add back db-less table methods

--- src/rethinkdb-crystal/api-db.cr
+++ src/rethinkdb-crystal/api-db.cr
@@ -5,6 +5,18 @@ module RethinkDB
     TableTerm.new(TermType::TABLE, [name])
   end
 
+  def self.table_create(name)
+    DatumTerm.new(TermType::TABLE_CREATE, [name])
+  end
+
+  def self.table_drop(name)
+    DatumTerm.new(TermType::TABLE_DROP, [name])
+  end
+
+  def self.table_list
+    DatumTerm.new(TermType::TABLE_LIST)
+  end
+
   class DBTerm < Term
     def table(name)
       TableTerm.new(TermType::TABLE, [self, name])
